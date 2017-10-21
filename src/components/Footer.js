42bf4d4fdfe47cb95f70deeccf88ecc8d2747188
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2017-10-21 11:04:21 -0400

    added some more contents

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-const Footer = () =>
-      <div>
-        <ul className="social">
-          <li><a href="https://github.com/hossainamzad"><i className="fa fa-github" aria-hidden="true"></i></a></li>
-          <li><a href="https://www.linkedin.com/in/hossainmdamzad/"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
-        </ul>
-        &copy; Amzad Hossain, 2017
-      </div>
+const Footer = () =>{
+  return (
+    <div>
+      <ul className="social">
+        <li><a href="https://github.com/hossainamzad"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+        <li><a href="https://github.com/hossainamzad"><i className="fa fa-github" aria-hidden="true"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/hossainmdamzad/"><i className="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+      </ul>
+      <h3>&copy; Amzad Hossain, 2017</h3>
+    </div>
+  )
+}
+
 
 export default Footer;
